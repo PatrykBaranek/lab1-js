05baa95b9920ac8d3411149acf673e29f8ca89b9
Patryk Baranek <cien2460045@gmail.com>
2022-10-21 10:27:20 +0200

    add working slider with intervals

--- lab1/slider/script.js
+++ lab1/slider/script.js
@@ -3,12 +3,16 @@ const images = [
 	'https://cdn.pixabay.com/photo/2019/08/19/07/45/corgi-4415649_960_720.jpg',
 	'https://cdn.pixabay.com/photo/2016/11/21/00/47/view-1844110_960_720.jpg',
 	'https://cdn.pixabay.com/photo/2016/11/01/10/29/dog-1787835_960_720.jpg',
+	'https://cdn.benchmark.pl/uploads/backend_img/c/recenzje/2022_10/12505_God_of_War_historia_Kratosa/GoW_Fenrir.jpg',
 	'https://preview.redd.it/p2ucani1xid81.jpg?width=640&crop=smart&auto=webp&s=cabafc1ce571ac8788fed4744305476a072d5133',
+	'https://whatifgaming.com/wp-content/uploads/2022/09/god-of-ragnarok.jpg',
+	'https://pliki.ppe.pl/storage/7a4bcdb491c8897a74b1/7a4bcdb491c8897a74b1.jpg',
+	'https://assets-prd.ignimgs.com/2022/09/13/screen-shot-2022-09-13-at-5-56-21-pm-1663109928557.png',
 ];
 
 let sliderPosition = 0;
 let sliderItems = [];
-const autoPhotoChanger = (func, time) => setInterval(func, time);
+let autoPhotoChangerId;
 
 const generateSlider = () => {
 	const slider = document.querySelector('.slider');
@@ -20,7 +24,6 @@ const generateSlider = () => {
 
 		const img = document.createElement('img');
 		img.src = item;
-		img.alt = 'dog photo';
 
 		slider.append(div);
 		div.append(img);
@@ -29,7 +32,7 @@ const generateSlider = () => {
 	document.querySelector('.slider__item').classList.remove('hide');
 
 	sliderItems = Array.from(document.querySelectorAll('.slider__item'));
-	autoPhotoChanger(nextPhotoHandler, 2000);
+	startAutoPhotoChanger();
 };
 
 const previousPhotoHandler = () => {
@@ -47,13 +50,6 @@ const previousPhotoHandler = () => {
 };
 
 const nextPhotoHandler = (e) => {
-	console.log(e);
-
-	if (e !== undefined) {
-		console.log('clearinterval');
-		clearInterval(autoPhotoChanger);
-	}
-
 	sliderItems[sliderPosition].classList.toggle('hide');
 
 	if (sliderPosition + 1 > sliderItems.length - 1) {
@@ -67,13 +63,22 @@ const nextPhotoHandler = (e) => {
 	sliderPosition++;
 };
 
-const sliderPhotoPosition = (sliderPosition) => {
-	let dotArray = Array(images.length);
-	dotArray.console.log(dotArray);
+const startAutoPhotoChanger = () => {
+	autoPhotoChangerId = setInterval(nextPhotoHandler, 2000);
 };
 
+// slider generator
 document.addEventListener('DOMContentLoaded', generateSlider);
 
+// turn on or off autoPhotoChanger
+document.querySelector('.slider').addEventListener('mouseover', () => {
+	clearInterval(autoPhotoChangerId);
+});
+document.querySelector('.slider').addEventListener('mouseleave', () => {
+	startAutoPhotoChanger();
+});
+
+// change photo by click
 document
 	.querySelector('.previous')
 	.addEventListener('click', previousPhotoHandler);
