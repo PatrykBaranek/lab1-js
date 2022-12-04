2b56e64bcd47d8a13aed89fd8d6e98bfbb149419
patrykbaranek <cien2460045@gmail.com>
2022-12-04 23:53:20 +0100

    refactor and finish notekeep

--- lab1/notekeep/src/helpers/addNote/postData.ts
+++ lab1/notekeep/src/helpers/addNote/postData.ts
@@ -1,22 +1,28 @@
 import { Note } from '../../model/Note.js';
-import { Color, DataFromForm } from '../../types/types.js';
+import { Color, DataFromForm, INote } from '../../types/types.js';
 import { closeAddForm } from './add-note-render.helper.js';
+
+const title = document.getElementById('title') as HTMLInputElement;
+const description = document.getElementById('description') as HTMLInputElement;
+const color = document.querySelector('select') as HTMLSelectElement;
+const isPined = document.getElementById('isPined') as HTMLInputElement;
 
 export const postData = (e: Event) => {
 	e.preventDefault();
-
-	const title = document.getElementById('title') as HTMLInputElement;
-	const description = document.getElementById(
-		'description'
-	) as HTMLInputElement;
-	const color = document.querySelector('select') as HTMLSelectElement;
-	const isPined = document.getElementById('isPined') as HTMLInputElement;
 
 	const selectedColor = color.options[color.selectedIndex].text;
 
 	const findColor = Object.values(Color).find(
 		(x) => x === selectedColor
 	) as Color;
+
+	if (
+		title.value.trim().length === 0 ||
+		description.value.trim().length === 0
+	) {
+		alert('Title or description cannot be empty!');
+		return;
+	}
 
 	const data: DataFromForm = {
 		title: title.value,
@@ -26,16 +32,18 @@ export const postData = (e: Event) => {
 	};
 
 	const newNote = new Note(
-		'',
 		data.title,
 		data.description,
 		data.color,
 		data.isPined
 	);
 
-	console.log(newNote);
-
 	newNote.createNewNote();
-
+	resetInputs();
 	closeAddForm();
+};
+
+const resetInputs = () => {
+	title.value = '';
+	description.value = '';
 };
